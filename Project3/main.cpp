eb82a523d567794b55f7ade3c8682e11a01a494b
kitkoss <@DESKTOP-LAGBP2N>
2024-09-19 14:13:37 +0500

    vot eto da

--- Project3/main.cpp
+++ Project3/main.cpp
@@ -1,2 +1,45 @@
-#include <iostram>
-std::cout << "Hello Word da";
+#include <iostream>
+
+int main() {
+    float num1, num2, result;
+    char znak;
+    std::cout << "Napishite pervoe chislo: ";
+    std::cin >> num1;
+
+    std::cout << "Napishite Znak (+, -, *, /, %): ";
+    std::cin >> znak;
+
+    std::cout << "Napishite vtoroe chislo: ";
+    std::cin >> num2;
+
+    if (znak== '+') {
+        result = num1 + num2;
+        std::cout << "Rezultat: " << result << std::endl;
+    }
+    else if (znak== '-') {
+        result = num1 - num2;
+        std::cout << "Rezultat: " << result << std::endl;
+    }
+    else if (znak == '*') {
+        result = num1 * num2;
+        std::cout << "Rezultat: " << result << std::endl;
+    }
+    else if (znak== '/') {
+        if (num2 != 0) {
+            result = num1 / num2;
+            std::cout << "Rezultat: " << result << std::endl;
+        }
+        else {
+            std::cout << "Oshibka: delenie nevozmohno na 0" << std::endl;
+        }
+    }
+    else if (znak== '%') {
+        result = num1 * (num2 / 100.0);
+        std::cout << "Rezultat: " << result << std::endl;
+    }
+    else {
+        std::cout << "Oshibka: ne tot znak" << std::endl;
+    }
+
+    return 0;
+}
